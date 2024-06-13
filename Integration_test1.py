3c831208afba08288b80364f708bbeba8568da66
ArfeenaJamal <ajamal7816@conestogac.on.ca>
2024-06-13 05:44:08 +0530

    ADD FILE

---
+++ Integration_test1.py
@@ -0,0 +1,12 @@
+import primenumberprogram;
+def test_is_prime_positive():
+    assert primenumberprogram.is_prime(17) == True  # Positive test case for prime number
+def test_is_prime_negative():
+    assert primenumberprogram.is_prime(15) == True  # Negative test case for non-prime number
+
+
+
+
+
+
+
